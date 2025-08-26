09b4e81769eb78b1c3d0f170d78c1392e44e5ffc
way <wayming.z@gmail.com>
2025-08-26 23:58:25 +1000

    image resitry

--- helm/install.sh
+++ helm/install.sh
@@ -5,8 +5,13 @@ sudo install minikube-linux-amd64 /usr/local/bin/minikube
 # 安装 kubectl
 sudo snap install kubectl --classic
 
-# 安装 argocd
-sudo snap install argocd --classic
+# 安装 argocd client
+VERSION=$(curl -L -s https://raw.githubusercontent.com/argoproj/argo-cd/stable/VERSION)
+curl -sSL -o argocd-linux-amd64 \
+  https://github.com/argoproj/argo-cd/releases/download/v$VERSION/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
+
 
 # 启动集群
 minikube start --cpus=4 --memory=8192 --driver=docker
@@ -25,12 +30,25 @@ curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 # 验证
 helm version
 
+# 添加 Helm repo
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+
+# 安装 ingress-nginx
+helm install ingress-nginx ingress-nginx/ingress-nginx \
+  -n ingress-nginx --create-namespace
+
+
+# 安装 metrics-server
+helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
+helm repo update
+helm install metrics-server metrics-server/metrics-server -n kube-system
+
+
 # 创建 argocd namespace
 kubectl create namespace argocd
-
 # 安装 argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
 # 获取初始密码
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
 
@@ -41,3 +59,6 @@ kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.pas
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add weaviate https://weaviate.github.io/weaviate-helm
 helm repo update
+
+# minikube docker shell
+# eval $(minikube -p minikube docker-env)
