e810e604ed40db6e8d09089994dac5139ea54abe
way <wayming.z@gmail.com>
2025-09-23 00:37:58 +1000

    pg connect

--- news_store/news_ingestor.py
+++ news_store/news_ingestor.py
@@ -1,6 +1,7 @@
 import os
 from common.mq_consumer import RabbitMQConsumer
 from .weaviate_writer import WeaviateWriter, WeaviateConfig
+from .pg_writer import PostgresWriter, PostgresConfig
 from contextlib import AsyncExitStack
 import asyncio
 from common.logger import SingletonLoggerSafe
@@ -28,11 +29,23 @@ async def main():
     )
     SingletonLoggerSafe.info(f"Connecting to RabbitMQ at {mq_config['host']}:{mq_config['queue_name']}")
 
+    pg_config = PostgresConfig(
+        host=os.getenv("PG_HOST", "postgres"),
+        port=os.getenv("PG_PORT", "5432"),
+        user=os.getenv("POSTGRES_USER", "postgres"),
+        password=os.getenv("POSTGRES_PASSWORD", "password"),
+        database=os.getenv("POSTGRES_DB", "postgres"),
+        table_name=os.getenv("PG_TABLE", "articles"),
+    )
+    SingletonLoggerSafe.info(f"Connecting to Postgres at {pg_config['host']}:{pg_config['port']}/{pg_config['database']}")
+
     async with AsyncExitStack() as stack:
         wv_client = await stack.enter_async_context(WeaviateWriter(weaviate_config))
+        pg_client = await stack.enter_async_context(PostgresWriter(pg_config))
         mq_consumer = await stack.enter_async_context(RabbitMQConsumer(mq_config))
 
         mq_consumer.with_handler(wv_client.store_article)
+        mq_consumer.with_handler(pg_client.store_article)
         await mq_consumer.consume() # wait until stop_event is set
 
 if __name__ == "__main__":
